dde32e407c7ef3a4359de1c7fd2bd101700b3498
Blessium <alessandro.melgar04@gmail.com>
2023-07-06 18:10:43 +0200

    Added sample api metrics

--- cmd/sample_prom/main.go
+++ cmd/sample_prom/main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
-    "github.com/gofiber/fiber/v2"
-    "github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+    "github.com/blessium/metricsgo/internal"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/valyala/fasthttp/fasthttpadaptor"
 )
 
 func main() {
-    app := fiber.New() 
+    app := fiber.New()
 
-    app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello World")
-    })
-    
-    app.Use(logger.New())
+    reg := internal.HandlerRandomMetrics()
+    prometheus.Register(reg)
 
-    app.Listen(":3000")
+	p := fasthttpadaptor.NewFastHTTPHandler(
+		promhttp.HandlerFor(
+			prometheus.DefaultGatherer,
+			promhttp.HandlerOpts{
+				EnableOpenMetrics: true,
+                Registry: reg,
+			},
+		),
+	)
+
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+
+    app.Get("/metrics", func (c *fiber.Ctx) error {
+        p(c.Context())
+        return nil
+    }) 
+
+	app.Use(logger.New())
+
+	app.Listen(":3000")
 }
+
--- internal/random_data.go
+++ internal/random_data.go
@@ -1,1 +1,36 @@
 package internal
+
+import (
+    "github.com/prometheus/client_golang/prometheus"
+    "math/rand"
+)
+
+type metrics struct {
+    cpuTemp prometheus.Gauge
+    hddTemp prometheus.Gauge
+}
+
+func HandlerRandomMetrics() *prometheus.Registry {
+    reg := prometheus.NewRegistry()
+    m := newMetric(reg)
+    m.cpuTemp.Set(rand.Float64())
+    m.hddTemp.Set(rand.Float64())
+    return reg
+}
+
+func newMetric(reg prometheus.Registerer) *metrics {
+    m := &metrics {
+        cpuTemp: prometheus.NewGauge(prometheus.GaugeOpts{
+            Name: "cpu_temperature_celsius",
+            Help: "Current temperature of the CPU",
+        }),
+        hddTemp: prometheus.NewGauge(prometheus.GaugeOpts{
+            Name: "hdd_temperature_celsius",
+            Help: "Current temperature of the HDD",
+        }),
+    }
+    reg.MustRegister(m.cpuTemp)
+    reg.MustRegister(m.hddTemp)
+
+    return m
+}
