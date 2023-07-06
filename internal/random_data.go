04bc46d869d2adca1814fcbaa6f3b2aa685d3d83
Blessium <alessandro.melgar04@gmail.com>
2023-07-07 00:16:25 +0200

    Fixed random data generator

--- internal/random_data.go
+++ internal/random_data.go
@@ -1,36 +1,49 @@
 package internal
 
 import (
-    "github.com/prometheus/client_golang/prometheus"
-    "math/rand"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"math/rand"
+	"time"
 )
 
 type metrics struct {
-    cpuTemp prometheus.Gauge
-    hddTemp prometheus.Gauge
+	cpuTemp prometheus.Gauge
+	hddTemp prometheus.Gauge
 }
 
 func HandlerRandomMetrics() *prometheus.Registry {
-    reg := prometheus.NewRegistry()
-    m := newMetric(reg)
-    m.cpuTemp.Set(rand.Float64())
-    m.hddTemp.Set(rand.Float64())
-    return reg
+	reg := prometheus.NewRegistry()
+	m := newMetric(reg)
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	bruh := func() {
+		for {
+			m.cpuTemp.Set(r.Float64())
+			m.hddTemp.Set(r.Float64())
+		}
+	}
+
+    go bruh()
+
+	return reg
+}
+
+func goRandomicNumberGen() {
+
 }
 
 func newMetric(reg prometheus.Registerer) *metrics {
-    m := &metrics {
-        cpuTemp: prometheus.NewGauge(prometheus.GaugeOpts{
-            Name: "cpu_temperature_celsius",
-            Help: "Current temperature of the CPU",
-        }),
-        hddTemp: prometheus.NewGauge(prometheus.GaugeOpts{
-            Name: "hdd_temperature_celsius",
-            Help: "Current temperature of the HDD",
-        }),
-    }
-    reg.MustRegister(m.cpuTemp)
-    reg.MustRegister(m.hddTemp)
-
-    return m
+	m := &metrics{
+		cpuTemp: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
+			Name: "cpu_temperature_celsius",
+			Help: "Current temperature of the CPU",
+		}),
+		hddTemp: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
+			Name: "hdd_temperature_celsius",
+			Help: "Current temperature of the HDD",
+		}),
+	}
+	return m
 }
