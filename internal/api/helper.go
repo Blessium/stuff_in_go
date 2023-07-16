ce5fe5782080a231f9dd7e7d6f429ba209c5e86e
Blessium <alessandro.melgar04@gmail.com>
2023-07-16 21:41:08 +0200

    Added json body decoder

--- internal/api/helper.go
+++ internal/api/helper.go
@@ -1,10 +1,33 @@
 package api
 
 import (
-    "net/http"
+	"errors"
+	"net/http"
+    "encoding/json"
+    "io"
 )
 
-// TODO: Complete JSON Body decoder for the handler
+const maxBodyBytes = 1048576
+
 func DecodeJSONBody(w http.ResponseWriter, r *http.Request, dst interface{}) error {
-    return nil
+	if r.Header.Get("Content-Type") != "application/json" {
+		return errors.New("Content-Type must be application/json")
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, maxBodyBytes)
+
+    dec := json.NewDecoder(r.Body)
+    dec.DisallowUnknownFields()
+
+    err := dec.Decode(&dst)
+    if err != nil {
+        return err
+    }
+
+    err = dec.Decode(&struct{}{})
+    if !errors.Is(err, io.EOF) {
+       return errors.New("Request body must contain only one entity") 
+    }
+
+	return nil
 }
